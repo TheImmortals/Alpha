18f5c8361a7c3d6c0c1f84653acf9894f0f8b846
FlamePrince-PS <44061946+FlamePrince-PS@users.noreply.github.com>
2018-10-21 03:41:13 +0000

    Add files via upload

--- mods/ssb/items.js
+++ mods/ssb/items.js
@@ -1,76 +1,104 @@
 'use strict';
 
-exports.BattleItems = {
-	//Tidal Wave Bot
-	guardiansamulet: {
-		id: "guardiansamulet",
-		name: "Guardian's Amulet",
-		onModifyDefPriority: 2,
-		onModifyDef: function (def, pokemon) {
-			if (pokemon.baseTemplate.nfe) {
-				return this.chainModify(1.5);
-			}
+/**@type {{[k: string]: ModdedItemData}} */
+let BattleItems = {
+	// E4 Flint
+	magmarizer: {
+		inherit: true,
+		megaStone: "Steelix-Mega",
+		megaEvolves: "Steelix",
+		onTakeItem: function (item, source) {
+			if (item.megaEvolves === source.baseTemplate.baseSpecies) return false;
+			return true;
 		},
-		onModifySpDPriority: 2,
-		onModifySpD: function (spd, pokemon) {
-			if (pokemon.baseTemplate.nfe) {
-				return this.chainModify(1.5);
-			}
-		},
-		onModifyDamage: function (damage, source, target, move) {
-			return this.chainModify([0x14CC, 0x1000]);
-		},
-		onAfterMoveSecondarySelf: function (source, target, move) {
-			if (source && source !== target && move && move.category !== 'Status' && !move.ohko) {
-				this.damage(source.maxhp / 10, source, source, this.getItem('lifeorb'));
-			}
-		},
+		fling: undefined, // Cannot be flung now since its a mega stone
+		desc: "If held by a Steelix, this item allows it to Mega Evolve in battle.",
 	},
-	"epicberry": {
-		id: "epicberry",
-		name: "Epic Berry",
-		spritenum: 217,
-		isBerry: true,
-		desc: "Power of a white herb, shuca berry, focus sash and pinch berry.",
-		naturalGift: {
-			basePower: 120,
-			type: "Dark",
-		},
-		onDamage: function (damage, target, source, effect) {
-			if (target.hp === target.maxhp && damage >= target.hp && effect && effect.effectType === 'Move' && !source.volatiles['sash']) {
-				source.addVolatile('sash');
-				return target.hp - 1;
-			}
-		},
-		onSourceModifyDamage: function (damage, source, target, move) {
-			if (move.type === 'Ground' && move.typeMod > 0 && (!target.volatiles['substitute'] || move.flags['authentic'] || (move.infiltrates && this.gen >= 6)) && !source.volatiles['shuca']) {
-				this.debug('-50% reduction');
-				this.add('-enditem', target, this.effect, '[weaken]');
-				source.addVolatile('shuca');
-				return this.chainModify(0.5);
-			}
-		},
-		onUpdate: function (pokemon, source) {
-			let activate = false;
-			let boosts = {};
-			for (let i in pokemon.boosts) {
-				if (pokemon.boosts[i] < 0) {
-					activate = true;
-					boosts[i] = 0;
-				}
-			}
-			if (activate && !source.volatiles['herb']) {
-				pokemon.setBoost(boosts);
-				this.add('-clearnegativeboost', pokemon, '[silent]');
-				pokemon.addVolatile('herb');
-			}
-			if (pokemon.hp <= pokemon.maxhp / 4 || (pokemon.hp <= pokemon.maxhp / 2 && pokemon.hasAbility('gluttony')) && !source.volatiles['med']) {
-				this.heal(pokemon.maxhp / 2);
-				pokemon.addVolatile('med');
-			}
-			if (source.volatiles['med'] && source.volatiles['herb'] && source.volatiles['shuca'] && source.volatiles['sash']) {
-				pokemon.useItem();
-			}
-		},
+	// FOMG
+	astleyiumz: {
+		id: "astleyiumz",
+		name: "Astleyium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Rickrollout",
+		zMoveFrom: "Rock Slide",
+		zMoveUser: ["Golem"],
+		gen: 7,
+		desc: "If held by a Golem with Rock Slide, it can use Rickrollout.",
+	},
+	// MajorBowman
+	victiniumz: {
+		id: "victiniumz",
+		name: "Victinium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Blaze of Glory",
+		zMoveFrom: "V-create",
+		zMoveUser: ["Victini"],
+		gen: 7,
+		desc: "If held by a Victini with V-create, it can use Blaze of Glory.",
+	},
+	// SamJo
+	thicciniumz: {
+		id: "thicciniumz",
+		name: "Thiccinium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Extra T h i c c",
+		zMoveFrom: "Thicc",
+		zMoveUser: ["Mamoswine"],
+		gen: 7,
+		desc: "If held by a Mamoswine with Thicc, it can use Extra T h i c c.",
+	},
+	// Snaquaza
+	fakeclaimiumz: {
+		id: "fakeclaimiumz",
+		name: "Fakeclaimium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Fake Claim",
+		// @ts-ignore hack for Snaquaza's Z move.
+		zMoveFrom: ["Brave Bird", "Superpower", "Sucker Punch", "Flamethrower", "Ice Beam", "Thunderbolt"],
+		zMoveUser: ["Honchkrow"],
+		gen: 7,
+		desc: "If held by a Honchkrow, it can use Fake Claim.",
+	},
+	// SunGodVolcarona
+	volcaroniumz: {
+		id: "volcaroniumz",
+		name: "Volcaronium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Scorching Global Vortex",
+		zMoveFrom: "Fiery Dance",
+		zMoveUser: ["Volcarona"],
+		gen: 7,
+		desc: "If held by a Volcarona with Fiery Dance, it can use Scorching Global Vortex.",
+	},
+	// Tiksi
+	tiksiumz: {
+		id: "tiksiumz",
+		name: "Tiksium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Devolution Wave",
+		zMoveFrom: "Rock Slide",
+		zMoveUser: ["Cradily"],
+		gen: 7,
+		desc: "If held by a Cradily with Rock Slide, it can use Devolution Wave.",
+	},
+	// Uselesscrab
+	nichiumz: {
+		id: "nichiumz",
+		name: "Nichium Z",
+		isNonstandard: true,
+		onTakeItem: false,
+		zMove: "Revamped Suspect Philosophy",
+		zMoveFrom: "Icicle Crash",
+		zMoveUser: ["Kyurem-Black"],
+		gen: 7,
+		desc: "If held by a Kyurem-Black with Icicle Crash, it can use Revamped Suspect Philosophy.",
 	},
 };
+
+exports.BattleItems = BattleItems;
