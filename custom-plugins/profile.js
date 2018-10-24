4f3a4b0f349fd3e799c5873b4bff266c397cd717
FlamePrince-PS <44061946+FlamePrince-PS@users.noreply.github.com>
2018-10-24 09:27:49 +0000

    Update profile.js

--- custom-plugins/profile.js
+++ custom-plugins/profile.js
@@ -544,9 +544,9 @@ exports.commands = {
 
 		let profileData = ``;
 		if (profile.background) {
-			profileData += `<div style="background:url(${profile.background}); background-size: 100% 100%; height: 200px overflow: auto">`;
+			profileData += `<div style="background:url(${profile.background}); background-size: 100% 100%; height: 150px; overflow: auto">`;
 		} else {
-			profileData += `<div style="max-height: 200px; overflow-y: scroll">`;
+			profileData += `<div style="max-height: 150px; overflow-y: auto">`;
 		}
 		profileData += `<div style="display: inline-block; width: 6.5em; height: 100%; vertical-align: top"><img src="${avatar}" height="80" width="80" align="left"></div>`;
 		profileData += `<div style="display: inline-block">&nbsp;${pColor(userid)}<strong>Name:</strong></font> ${Server.nameColor(username, true)}&nbsp;`;
