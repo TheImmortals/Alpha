0684286c3dfc80a5f92b0c5a741ef6c7c9720b3a
Akash Kumar <34735962+AllianceSky-PS@users.noreply.github.com>
2018-10-31 17:19:41 +0000

    Update misc.js

--- custom-plugins/misc.js
+++ custom-plugins/misc.js
@@ -8,14 +8,6 @@
  */
 'use strict';
 
-/*let monData;
-
-try {
-	monData = FS("data/wlssb-data.txt").readIfExistsSync().toString().split("\n\n");
-} catch (e) {
-	console.error(e);
-}*/
-
 const bubbleLetterMap = new Map([
 	["a", "\u24D0"], ["b", "\u24D1"], ["c", "\u24D2"], ["d", "\u24D3"], ["e", "\u24D4"], ["f", "\u24D5"], ["g", "\u24D6"], ["h", "\u24D7"], ["i", "\u24D8"], ["j", "\u24D9"], ["k", "\u24DA"], ["l", "\u24DB"], ["m", "\u24DC"],
 	["n", "\u24DD"], ["o", "\u24DE"], ["p", "\u24DF"], ["q", "\u24E0"], ["r", "\u24E1"], ["s", "\u24E2"], ["t", "\u24E3"], ["u", "\u24E4"], ["v", "\u24E5"], ["w", "\u24E6"], ["x", "\u24E7"], ["y", "\u24E8"], ["z", "\u24E9"],
@@ -46,19 +38,6 @@ function parseStatus(text, encoding) {
 	}
 	return text;
 }
-
-/*function getMonData(target) {
-	let returnData = null;
-	if (!monData) return 'Data not found';
-	monData.forEach(function (data) {
-		if (toId(data.split("\n")[0].split(" - ")[0] || " ") === target) {
-			returnData = data.split("\n").map(function (line) {
-				return Chat.escapeHTML(line);
-			}).join("<br />");
-		}
-	});
-	return returnData;
-}*/
 
 function clearRoom(room) {
 	let len = (room.log.log && room.log.log.length) || 0;
@@ -183,48 +162,6 @@ exports.commands = {
 			'<hr>' +
 			'<b>Administrators (~)</b> - Administrators are to be contacted if there is a serious issue. This may include sexual harrassment and/or abuse of power from Room Owners as well as Global Staff. Its also important to contact Administrators if there are any bugs within the server system that need to be looked at.  <br />'
 		);
-	},
-
-	chatcolor: 'cc',
-	cc: {
-		give: function (target, room, user) {
-			if (!this.can('ban')) return false;
-			if (!target) return this.errorReply('USAGE: /chatcolor give [USER]');
-			Db.chatcolors.set(target, 1);
-			this.sendReply(target + ' has been given the ability to use chat colors.');
-			Users(target).popup('You have been given the ability to use chat color.');
-		},
-		take: function (target, room, user) {
-			if (!this.can('ban')) return false;
-			if (!target) return this.errorReply('USAGE: /chatcolor take [USER]');
-			if (!Db.chatcolors.has(user)) return this.errorReply('This user does not have the ability to use chat colors.');
-			Db.chatcolors.remove(user);
-			this.sendReply('this user has had their ability to use chat colors is taken from them.');
-		},
-		u: 'use',
-		use: function (target, room, user) {
-			let group = user.getIdentity().charAt(0);
-			if (room.auth) group = room.auth[user.userid] || group;
-			if (user.hiding) group = ' ';
-			let targets = target.split(',');
-			if (targets.length < 2) return this.parse('/chatcolor help');
-			if (!Db.chatcolors.has(user.userid)) return this.errorReply('You dont have ability to use chat colors.');
-			if (!this.canTalk()) return this.errorReply("You may not use this command while unable to speak.");
-			this.add('|raw|' + "<small>" + group + "</small>" + "<button name='parseCommand' value='/user " + user.name + "' style='background: none ; border: 0 ; padding: 0 5px 0 0 ; font-family: &quot;verdana&quot; , &quot;helvetica&quot; , &quot;arial&quot; , sans-serif ; font-size: 9pt ; cursor: pointer'>" + Server.nameColor(user.name, true) + ":</button>" + '<b><font color="' + targets[0].toLowerCase().replace(/[^#a-z0-9]+/g, '') + '">' + Chat.escapeHTML(targets.slice(1).join(",")) + '</font></b>');
-		},
-		'': 'help',
-		help: function (target, user, room) {
-			if (!this.runBroadcast()) return;
-			this.sendReplyBox(
-				'<center><u><b>Chat Color By Prince Sky</b></u><br>' +
-                'All commands is nestled under namespace <code>chatcolor</code><center>' +
-                '<hr width="80%">' +
-                '<code>give [user] </code>- Give user ability to use chat color. Requires @ or higher.<br>' +
-                '<code>take [user] </code>- Take user\'s ability of using chat color. Requires @ or higher.<br>' +
-                '<code>use [color], [msg] </code>- Post colored msg in chat.<br>' +
-                '<code>help</code> - Displays this command.'
-			);
-		},
 	},
 
 	roomlist: function (target, room, user) {
@@ -593,28 +530,6 @@ exports.commands = {
 		if ((86400000 - nextBonus) <= 0) return Server.giveDailyReward(user);
 		return this.sendReply('Your next bonus is ' + obj[0] + ' ' + (obj[0] === 1 ? currencyName : currencyPlural) + ' in ' + Chat.toDurationString(Math.abs(86400000 - nextBonus)));
 	},
-
-	disableintroscroll: function (target, room, user) {
-		if (!this.can('makeroom')) return false;
-		if (!target) return this.errorReply("No Room Specified");
-		target = toId(target);
-		if (!Rooms(target)) return this.errorReply(`${target} is not a room`);
-		if (Db.disabledScrolls.has(target)) return this.errorReply(`${Rooms(target).title} has roomintro scroll disabled.`);
-		Db.disabledScrolls.set(target, true);
-		Monitor.adminlog(user.name + ` has disabled the roomintro scroll bar for ${Rooms(target).title}.`);
-	},
-
-	disableintroscrollhelp: ["/disableintroscroll [room] - Disables scroll bar preset in the room's roomintro."],
-	enableintroscroll: function (target, room, user) {
-		if (!this.can('makeroom')) return false;
-		if (!target) return this.errorReply("No Room Specified");
-		target = toId(target);
-		if (!Rooms(target)) return this.errorReply(`${target} is not a room`);
-		if (!Db.disabledScrolls.has(target)) return this.errorReply(`${Rooms(target).title} has roomintro scroll enabled.`);
-		Db.disabledScrolls.remove(target);
-		Monitor.adminlog(user.name + ` has enabled the roomintro scroll bar for ${Rooms(target).title}.`);
-	},
-	enableintroscrollhelp: ["/enableintroscroll [room] - Enables scroll bar preset in the room's roomintro."],
 
 	pmroom: 'rmall',
 	roompm: 'rmall',
