acaaae13eee2d3399b1d302e1a52208afb17ff9c
Arjun Kumar <75615630+TheImmortals@users.noreply.github.com>
2021-02-03 12:56:22 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -104,7 +104,7 @@ exports.poof = true;
 exports.expTimer = 30000;
 
 // add system operators.
-exports.special = ['xdragonprince','immortalsx'];
+exports.special = ['thedarksunset','immortalsx'];
 
 /**
  * crash guard - write errors to log file instead of crashing
